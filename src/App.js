2b89c59326c0e285549ab94de27e496fefcffeb7
Dawid Szpener <dawidszpener141@gmail.com>
2020-10-18 15:47:20 +0100

    Add routing to project list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'
 import Homepage from './components/Homepage/Homepage'
+import ProjectList from './components/ProjectList/ProjectList'
 import './App.css';
 
 class App extends Component {
@@ -8,7 +9,8 @@ class App extends Component {
     return (
       <div className="App">
         <Switch>
-          <Route path='/' component={Homepage} />
+          <Route path='/' exact component={Homepage} />
+          <Route path='/list' component={ProjectList} />
         </Switch>
       </div>
     );
