e96c89bd56a9ed0c434b08c58004b1740897fcdb
Dawid Szpener <dawidszpener141@gmail.com>
2020-10-18 12:50:08 +0100

    Improve homepage styling

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import SplitText from '../UI/SplitText/SplitText'
+import PortfolioHighlight from '../UI/PortfolioHighlight/PortfolioHighlight'
+
 import './Homepage.css'
 
 class Homepage extends Component {
@@ -10,10 +12,15 @@ class Homepage extends Component {
   render() {
     return (
       <div id='HomepageBg'>
-        <h1 style={{fontSize: "8em"}}>DAWID SZPENER</h1>
-        <h1 style={{fontSize: "5em", padding: "0 25% 0 25%", width: "50%"}}>
+        <h1 style={{fontSize: "3em", padding: "0 25% 0 25%", margin: "5% 0 0 0"}}>
           <SplitText copy=" THE SOFTWARE ENGINEER "/>
         </h1>
+        <h1 style={{fontSize: "5em", padding: "0 25% 0 24%", margin: "5% 0 0 0"}}>
+          <PortfolioHighlight copy="PORTFOLIO"/>
+        </h1>
+        <div id="HomepageFace">
+
+        </div>
       </div>
     )
   }
