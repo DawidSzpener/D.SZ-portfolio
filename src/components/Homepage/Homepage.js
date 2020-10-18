8332772a559beaed6feb381f30f45e04a111a09d
Dawid Szpener <dawidszpener141@gmail.com>
2020-10-18 14:41:54 +0100

    Add homepage picture

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -13,13 +13,12 @@ class Homepage extends Component {
     return (
       <div id='HomepageBg'>
         <h1 style={{fontSize: "3em", padding: "0 25% 0 25%", margin: "5% 0 0 0"}}>
-          <SplitText copy=" THE SOFTWARE ENGINEER "/>
+          <SplitText copy=" THE SOFTWARE ENGINEER'S "/>
         </h1>
         <h1 style={{fontSize: "5em", padding: "0 25% 0 24%", margin: "5% 0 0 0"}}>
           <PortfolioHighlight copy="PORTFOLIO"/>
         </h1>
         <div id="HomepageFace">
-
         </div>
       </div>
     )
