957263970b9285801a3e61a7581c3e51a6f37f1a
Dawid Szpener <dawidszpener141@gmail.com>
2020-11-07 11:03:14 +0000

    Unignore build folder

---
+++ build/precache-manifest.f63e403757da475f87c58bafa815d5f3.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5c1503eab6a7e769f3cc26c3116091",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fece5c8b2113f18a3440",
+    "url": "/static/css/2.05a0ae32.chunk.css"
+  },
+  {
+    "revision": "50dfc9fca1233d3ff716",
+    "url": "/static/css/main.f026b468.chunk.css"
+  },
+  {
+    "revision": "fece5c8b2113f18a3440",
+    "url": "/static/js/2.f87195c6.chunk.js"
+  },
+  {
+    "revision": "857d129e4a5f4f33c7a4a143bf79109c",
+    "url": "/static/js/2.f87195c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50dfc9fca1233d3ff716",
+    "url": "/static/js/main.113fc637.chunk.js"
+  },
+  {
+    "revision": "caefb24eda7e785879e9",
+    "url": "/static/js/runtime-main.0e998ba7.js"
+  },
+  {
+    "revision": "6f3cc73165cbad258249336111584ab7",
+    "url": "/static/media/Cellular-Automaton.6f3cc731.gif"
+  },
+  {
+    "revision": "fa984cef91f37cf82e3b3c08f469d4a0",
+    "url": "/static/media/acebook.fa984cef.gif"
+  },
+  {
+    "revision": "66efe2289450e4dceec41cee6e6879b0",
+    "url": "/static/media/burger.66efe228.gif"
+  },
+  {
+    "revision": "04a8c60f57d49ad32acb2b7241ee44cb",
+    "url": "/static/media/cooking01.04a8c60f.jpg"
+  },
+  {
+    "revision": "4d38d100dc41639c6b916a392dd83f9c",
+    "url": "/static/media/cooking02.4d38d100.jpg"
+  },
+  {
+    "revision": "90bf17f648a3a17cb771e48fbb0a5d39",
+    "url": "/static/media/face.90bf17f6.png"
+  },
+  {
+    "revision": "996f3f2e05f58d25723362c65b7a9468",
+    "url": "/static/media/fishing01.996f3f2e.jpeg"
+  },
+  {
+    "revision": "48ad6784654dba352cc86a5ae5cd50e5",
+    "url": "/static/media/fishing02.48ad6784.jpeg"
+  },
+  {
+    "revision": "1bfbc1ca00beef9b7b991ce484c2aa57",
+    "url": "/static/media/ja01.1bfbc1ca.jpg"
+  },
+  {
+    "revision": "bd615c25828b264f6016c81320bb6ecd",
+    "url": "/static/media/ja02.bd615c25.jpg"
+  },
+  {
+    "revision": "fbaf6bb2b0dd7a5d8c4f623de6f7b571",
+    "url": "/static/media/makers02.fbaf6bb2.jpg"
+  },
+  {
+    "revision": "2de5ad9befc6980a0b7280e57eae8b61",
+    "url": "/static/media/makers03.2de5ad9b.jpg"
+  },
+  {
+    "revision": "2f2ffc68d09e82ecf287ce12bef25797",
+    "url": "/static/media/ricipici.2f2ffc68.gif"
+  },
+  {
+    "revision": "e459f99d96bc8d88deb3829a09e78d32",
+    "url": "/static/media/sas.e459f99d.gif"
+  },
+  {
+    "revision": "af40044284dac3200cc36eb037ccd555",
+    "url": "/static/media/sayanface.af400442.png"
+  },
+  {
+    "revision": "d09a2670855a388a9f6f1cd78e5708c3",
+    "url": "/static/media/strzal01.d09a2670.jpg"
+  },
+  {
+    "revision": "ea2767ab552050f9962a5c23f7e227cf",
+    "url": "/static/media/strzal02.ea2767ab.jpg"
+  },
+  {
+    "revision": "af8b58f7d5f1ffcd9ada608b10d97fa0",
+    "url": "/static/media/travel01.af8b58f7.png"
+  },
+  {
+    "revision": "afee3e9e3662f8923b4396698edf8343",
+    "url": "/static/media/travel02.afee3e9e.jpg"
+  }
+]);
